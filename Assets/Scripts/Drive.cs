894d4d17eb1c28793cdd432b86ccfe90f330458a
Fazlul69 <sawravm299@gmail.com>
2020-02-25 15:29:07 +0600

    Initial commit

---
+++ Assets/Scripts/Drive.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drive : MonoBehaviour
+{
+    public WheelCollider wc;
+    public float torque = 200;
+    public GameObject wheel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        wc = this.GetComponent<WheelCollider>();
+    }
+
+    void Go(float accel)
+    {
+        accel = Mathf.Clamp(accel, -1, 1);
+        float thrustTorque = accel * torque;
+        wc.motorTorque = thrustTorque;
+
+        Quaternion quaternion;
+        Vector3 position;
+        wc.GetWorldPose(out position, out quaternion);
+        wheel.transform.position = position;
+        wheel.transform.rotation = quaternion;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float a = Input.GetAxis("Vertical");
+        Go(a);
+    }
+}
