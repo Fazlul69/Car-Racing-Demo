a5aca194d60f633bff0e8256d7cd83006495e9ef
Fazlul69 <sawravm299@gmail.com>
2020-02-25 15:57:24 +0600

    wheel control properly

--- Assets/Scripts/Drive.cs
+++ Assets/Scripts/Drive.cs
@@ -4,32 +4,43 @@ using UnityEngine;
 
 public class Drive : MonoBehaviour
 {
-    public WheelCollider wc;
+    public WheelCollider[] wc;
     public float torque = 200;
-    public GameObject wheel;
+    public GameObject[] wheel;
+    public float maxStreetAngle = 30;
     // Start is called before the first frame update
     void Start()
     {
-        wc = this.GetComponent<WheelCollider>();
+       // wc = this.GetComponent<WheelCollider>();
     }
 
-    void Go(float accel)
+    void Go(float accel, float steer)
     {
         accel = Mathf.Clamp(accel, -1, 1);
+        steer = Mathf.Clamp(steer, -1, 1) * maxStreetAngle;
         float thrustTorque = accel * torque;
-        wc.motorTorque = thrustTorque;
+        for (int i = 0; i < wc.Length; i++) {
+            wc[i].motorTorque = thrustTorque;
+            if (i < 2) {
+                wc[i].steerAngle = steer;
+            }
+        
 
-        Quaternion quaternion;
-        Vector3 position;
-        wc.GetWorldPose(out position, out quaternion);
-        wheel.transform.position = position;
-        wheel.transform.rotation = quaternion;
+            Quaternion quaternion;
+            Vector3 position;
+            wc[i].GetWorldPose(out position, out quaternion);
+
+            wheel[i].transform.position = position;
+            wheel[i].transform.rotation = quaternion;
+        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         float a = Input.GetAxis("Vertical");
-        Go(a);
+        float s = Input.GetAxis("Horizontal");
+        Go(a,s);
     }
 }
