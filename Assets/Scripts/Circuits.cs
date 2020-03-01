d02083fe6d2ad65e494da95ccab112768c6afe28
Fazlul69 <sawravm299@gmail.com>
2020-03-01 15:25:26 +0600

    add waypoint
    bu using AI , car running in the direction of waypoints

---
+++ Assets/Scripts/Circuits.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circuits : MonoBehaviour
+{
+    public GameObject[] waypoints;
+
+    void OnDrawGizmos() {
+        DrawGizmoz(false);
+    }
+
+    void OnDrawGizmozSelected()
+    {
+        DrawGizmoz(true);
+    }
+
+    void DrawGizmoz(bool selected) {
+        if (selected == false)
+        { return; }
+        if (waypoints.Length > 1)
+        {
+            Vector3 prev = waypoints[0].transform.position;
+            for (int i = 1; i < waypoints.Length; i++)
+            {
+                Vector3 next = waypoints[i].transform.position;
+                Gizmos.DrawLine(prev, next);
+                prev = next;
+            }
+            Gizmos.DrawLine(prev, waypoints[0].transform.position);
+        }
+    }
+}
